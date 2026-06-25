5812beee3b118a46e58868b2fd249d03f2bdca59
li19359129850 <138029897+li19359129850@users.noreply.github.com>
2026-06-26 03:50:53 +0800

    Update MixinInfoToggle.java

--- common/src/main/java/com/ecaree/minihudextra/mixin/moreInfo/MixinInfoToggle.java
+++ common/src/main/java/com/ecaree/minihudextra/mixin/moreInfo/MixinInfoToggle.java
@@ -1,8 +1,13 @@
 package com.ecaree.minihudextra.mixin.moreInfo;
 
 import com.ecaree.minihudextra.config.MHExInfoToggle;
+import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 import fi.dy.masa.minihud.config.InfoToggle;
-import org.spongepowered.asm.mixin.*;
+import fi.dy.masa.minihud.info.InfoLineType;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,19 +25,54 @@ public class MixinInfoToggle {
     private static InfoToggle[] $VALUES;
 
     @Invoker("<init>")
-    private static InfoToggle minihudextra$invokeInit(String enumName, int enumOrdinal, String name, InfoLineType type, boolean defaultValue, boolean serverDataRequired, int linePosition, String defaultHotkey, String comment, KeybindSettings settings, String translatedName, String prettyName) {
+    private static InfoToggle minihudextra$invokeInit(
+            String enumName,
+            int enumOrdinal,
+            String name,
+            InfoLineType type,
+            boolean defaultValue,
+            boolean serverDataRequired,
+            int linePosition,
+            String defaultHotkey,
+            String comment,
+            KeybindSettings settings,
+            String translatedName,
+            String prettyName
+    ) {
         throw new AssertionError();
     }
 
-    @Inject(method = "<clinit>", at = @At(value = "FIELD", target = "Lfi/dy/masa/minihud/config/InfoToggle;$VALUES:[Lfi/dy/masa/minihud/config/InfoToggle;", shift = At.Shift.AFTER))
+    @Inject(
+            method = "<clinit>",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lfi/dy/masa/minihud/config/InfoToggle;$VALUES:[Lfi/dy/masa/minihud/config/InfoToggle;",
+                    shift = At.Shift.AFTER
+            )
+    )
     private static void addCustomInfo(CallbackInfo ci) {
         List<InfoToggle> infos = new ArrayList<>(Arrays.asList($VALUES));
         InfoToggle last = infos.get(infos.size() - 1);
+
         int i = 1;
         for (MHExInfoToggle info : MHExInfoToggle.values()) {
-            infos.add(minihudextra$invokeInit(info.name(), last.ordinal() + i, info.getName(), null, info.getDefaultBooleanValue(), false, info.getLinePosition(), "", info.getComment(), KeybindSettings.DEFAULT, info.getName(), info.getName()));
+            infos.add(minihudextra$invokeInit(
+                    info.name(),
+                    last.ordinal() + i,
+                    info.getName(),
+                    null,
+                    info.getDefaultBooleanValue(),
+                    false,
+                    info.getLinePosition(),
+                    "",
+                    info.getComment(),
+                    KeybindSettings.DEFAULT,
+                    info.getName(),
+                    info.getName()
+            ));
             i++;
         }
+
         $VALUES = infos.toArray(new InfoToggle[0]);
     }
 }
